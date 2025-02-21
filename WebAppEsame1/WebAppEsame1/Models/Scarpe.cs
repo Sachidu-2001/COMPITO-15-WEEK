0018362319b258eb21aaf097a603c95030ab5c16
Sachidu-2001 <fsachi0001@gmail.com>
2025-02-21 13:31:59 +0100

    repository complete

--- WebAppEsame1/WebAppEsame1/Models/Scarpe.cs
+++ WebAppEsame1/WebAppEsame1/Models/Scarpe.cs
@@ -7,9 +7,23 @@
             new Product()
             {
                 Id = 1,
-                Nome_
+                Nome = "NikeAirForceBlack",
+                Prezzo = 120.99m,
+                Descrizione = "Nike Snweakers Air Force da Uomo",
+                Immagine_Copertina = "/Images/nikeairfoceblack.webp",
+                Img_1 = "/Images/img1.jpeg",
+                Img_2 = "/Images/img2.avif"
+            },
+            new Product() {
+                Id = 2,
+                Nome = "AdidasRed",
+                Prezzo = 79.99m,
+                Descrizione = "Adidas Sneakers Rosse da Uomo",
+                Immagine_Copertina = "/Images/adidasred.webp",
+                Img_1 = "/Images/img3.webp",
+                Img_2 = "/Images/img4.webp"
             }
-        }
+        };
     }
 
 }
