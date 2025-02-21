621c081f80ff89eb7e8b77d6d383ec3aa5070ed8
Sachidu-2001 <fsachi0001@gmail.com>
2025-02-21 14:38:20 +0100

    improving database

--- WebAppEsame1/WebAppEsame1/Models/Scarpe.cs
+++ WebAppEsame1/WebAppEsame1/Models/Scarpe.cs
@@ -22,6 +22,27 @@
                 Immagine_Copertina = "/Images/adidasred.webp",
                 Img_1 = "/Images/img3.webp",
                 Img_2 = "/Images/img4.webp"
+            },
+            new Product()
+            {
+                Id = 3,
+                Nome = "Yeezy Boost 350 Black&Blue",
+                Prezzo = 339.00m,
+                Descrizione = "Yeezy Boost 350 V2 Dazzling Blue",
+                Immagine_Copertina = "/Images/yeezyboost.webp",
+                Img_1 = "/Images/img5.webp",
+                Img_2 = "/Images/img6.webp"
+            },
+
+            new Product()
+            {
+                Id= 4,
+                Nome = "Lumberjack Boots",
+                Prezzo = 54.99m,
+                Descrizione = "Lumberjack River Boot Fur Lining Giallo Marrone",
+                Immagine_Copertina = "/Images/lumberjack.jpeg",
+                Img_1 = "/Images/img7.jpeg",
+                Img_2 = "/Images/img8.jpg"
             }
         };
     }
