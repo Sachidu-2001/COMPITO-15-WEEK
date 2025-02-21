01ed8db28935de4e80955b58529904cb6a5dc24d
Sachidu-2001 <fsachi0001@gmail.com>
2025-02-21 10:29:44 +0100

    commit

--- WebAppEsame1/WebAppEsame1/Controllers/HomeController.cs
+++ WebAppEsame1/WebAppEsame1/Controllers/HomeController.cs
@@ -1,8 +1,8 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using WebAppEsame.Models;
+using WebAppEsame1.Models;
 
-namespace WebAppEsame.Controllers
+namespace WebAppEsame1.Controllers
 {
     public class HomeController : Controller
     {
--- WebAppEsame1/WebAppEsame1/Models/ErrorViewModel.cs
+++ WebAppEsame1/WebAppEsame1/Models/ErrorViewModel.cs
@@ -1,4 +1,4 @@
-namespace WebAppEsame.Models
+namespace WebAppEsame1.Models
 {
     public class ErrorViewModel
     {
